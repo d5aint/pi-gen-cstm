abdec02d980dbff7edca0c1200bf3062ee9b1a54
d5aint <mike@dsaint.net>
2025-11-01 14:45:55 -0700

    Backup: updates
    Commit triggered on 2025-11-01 14:45:55

--- stage2/05-xtra-packages/00-run.sh
+++ stage2/05-xtra-packages/00-run.sh
@@ -1,5 +1,30 @@
 #!/bin/bash -e
 
+install -m 644 files/40-local-basic-rules.rules "${ROOTFS_DIR}/etc/polkit-1/rules.d/"
+
+#if [ -f "${ROOTFS_DIR}/etc/update-motd.d/10-uname" ]; then
+#    rm "${ROOTFS_DIR}/etc/update-motd.d/10-uname"
+#    mkdir "${ROOTFS_DIR}/etc/update-motd-static.d"
+#fi
+
+#touch "${ROOTFS_DIR}/etc/update-motd.d/20-update"
+#chmod 755 "${ROOTFS_DIR}/etc/update-motd.d/20-update"
+
+#install -m 755 files/10-welcome "${ROOTFS_DIR}/etc/update-motd.d/"
+#install -m 755 files/15-system "${ROOTFS_DIR}/etc/update-motd.d/"
+#install -m 755 files/20-update "${ROOTFS_DIR}/etc/update-motd-static.d/"
+
+#install -m 644 files/motd-update.timer "${ROOTFS_DIR}/etc/systemd/system/"
+#install -m 755 files/motd-update.service "${ROOTFS_DIR}/etc/systemd/system/"
+
+#sed -i -Ee 's/^#?[[:blank:]]*PrintLastLog[[:blank:]]*yes[[:blank:]]*$/PrintLastLog no/' \
+#  "${ROOTFS_DIR}/etc/ssh/sshd_config"
+
+#on_chroot << EOF
+#systemctl enable motd-update.timer
+#run-parts /etc/update-motd-static.d
+#EOF
+
 on_chroot << EOF
 #apt-mark manual 
 #apt-get -y purge 
--- stage3/00-install-packages/00-run.sh
+++ stage3/00-install-packages/00-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-install -v -m 0755 -o 1000 -g 1000 -d              "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.config/{i3/scripts,rofi}
+install -v -m 0755 -o 1000 -g 1000 -d              "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/{i3/scripts,rofi}"
 install -m 664 -o 1000 -g 1000 files/config        "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/i3/"
 install -m 644 -o 1000 -g 1000 files/wallpaper.jpg "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/i3/"
 install -m 755 -o 1000 -g 1000 files/power-menu.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/i3/scripts/"
 install -m 664 -o 1000 -g 1000 files/config.rasi   "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/rofi/"
+
+on_chroot << EOF
+xdg-user-dirs-update
+EOF
